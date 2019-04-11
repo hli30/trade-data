a209bf8f6a68a7550080c9874000bebea1fe20af
Harry L <harrylll93@gmail.com>
2019-04-11 08:32:29 -0700

    added mongo test connection

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,17 @@
-import express = require('express')
+import express from 'express';
+import mongoose from 'mongoose';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
+mongoose.connect('mongodb://localhost/test', {useNewUrlParser: true});
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function() {
+    console.log('connected');
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
